d55d9f641d4e7146713d5a03ec69a14d49680055
Fespermano <ottofesperman@gmail.com>
2024-11-07 11:27:54 -0500

    Comments Added
    Descriptive comments on code

--- CPXArduino_OttoF.ino
+++ CPXArduino_OttoF.ino
@@ -18,13 +18,13 @@ void loop() {
   // Left Button Halloween lights
   Serial.print("Left Button: ");
   if (leftButtonPressed) {
-    //P1 SET A
+    //P1 SET A 
     CircuitPlayground.setPixelColor(0, 0xFF6600); //Orange
     CircuitPlayground.setPixelColor(1, 0x800080); //purple
     CircuitPlayground.setPixelColor(2, 0x008000); //green
     CircuitPlayground.setPixelColor(3, 0xFF6600); //orange
     CircuitPlayground.setPixelColor(4, 0x800080); //purple
-    CircuitPlayground.setPixelColor(5, 0x008000); //green
+    CircuitPlayground.setPixelColor(5, 0x008000); //green.   // This code alternates between three different light sets (A, B and C)
     CircuitPlayground.setPixelColor(6, 0xFF6600); //orange
     CircuitPlayground.setPixelColor(7, 0x800080); //purple
     CircuitPlayground.setPixelColor(8, 0x008000); //green 
@@ -101,11 +101,11 @@ void loop() {
     CircuitPlayground.setPixelColor(7, 0x800080); //purple
     CircuitPlayground.setPixelColor(8, 0x008000); //green 
     CircuitPlayground.setPixelColor(9, 0xFF6600); //orange
-    Serial.print("UP");
+    Serial.print("Halloween");
 
   } else {
 
-    Serial.print("A");
+    Serial.print("WorkingH");
   }
 // Right Button Christmas Lights
   Serial.print("   Right Button: ");
@@ -114,7 +114,7 @@ void loop() {
     CircuitPlayground.setPixelColor(0, 0xFF0000); //Red // 
     CircuitPlayground.setPixelColor(1, 0xFFFFFF); //White
     CircuitPlayground.setPixelColor(2, 0x008000); //green
-    CircuitPlayground.setPixelColor(3, 0xFF0000); //Red
+    CircuitPlayground.setPixelColor(3, 0xFF0000); //Red        // This code alternates between three different light sets (A, B and C)
     CircuitPlayground.setPixelColor(4, 0xFFFFFF); //White
     CircuitPlayground.setPixelColor(5, 0x008000); //green
     CircuitPlayground.setPixelColor(6, 0xFF0000); //Red
@@ -123,12 +123,12 @@ void loop() {
     CircuitPlayground.setPixelColor(9, 0xFF0000); //Red
     delay(800);
     //SET B
-    CircuitPlayground.setPixelColor(1, 0xFF0000); //Red //I reordered the placement of colors instead of changeing colors.  
+    CircuitPlayground.setPixelColor(1, 0xFF0000); //Red        //I reordered the placement of colors instead of changeing colors here.   
     CircuitPlayground.setPixelColor(0, 0xFFFFFF); //White
     CircuitPlayground.setPixelColor(2, 0x008000); //green
     CircuitPlayground.setPixelColor(4, 0xFF0000); //Red
     CircuitPlayground.setPixelColor(3, 0xFFFFFF); //White
-    CircuitPlayground.setPixelColor(5, 0x008000); //green
+    CircuitPlayground.setPixelColor(5, 0x008000); //green.     
     CircuitPlayground.setPixelColor(7, 0xFF0000); //Red
     CircuitPlayground.setPixelColor(6, 0xFFFFFF); //White
     CircuitPlayground.setPixelColor(8, 0x008000); //green 
@@ -194,11 +194,11 @@ void loop() {
     CircuitPlayground.setPixelColor(8, 0x008000); //green 
     CircuitPlayground.setPixelColor(9, 0xFF0000); //Red
     delay(800);
-    Serial.print("DOWN");
+    Serial.print("Christmas");
 
   } else {
 
-    Serial.print("B"); 
+    Serial.print("WorkingC"); 
   }
     Serial.println();
     delay(4000);   
